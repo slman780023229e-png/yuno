b54d2c29879f905c2e32c077bc011e51bf4a811d
slman780023229e-png <slman780023229e@gmail.com>
2026-07-22 11:20:16 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+export default {
+
+    botName: "𝐘𝐔𝐍𝐎",
+
+    owner: [
+        "967783028397"
+    ],
+
+    prefix: ".",
+
+    sessionPath: "./session",
+
+    GEMINI_KEY: "AQ.Ab8RN6IXe95FL7lSlN_VbMiwtvlw-ObLtJL1CbwyVcQM7mOeLQ"
+
+};
